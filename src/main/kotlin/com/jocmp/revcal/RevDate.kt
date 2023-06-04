b7ba260934695d8cd5d1f52c9d3fb906d2921ae3
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-06-03 21:25:20 -0500

    Implement calendar

--- src/main/kotlin/com/jocmp/revcal/RevDate.kt
+++ src/main/kotlin/com/jocmp/revcal/RevDate.kt
@@ -2,10 +2,42 @@ package com.jocmp.revcal
 
 import java.time.LocalDate
 
-data class RevDate(val year: Int, val month: Int, val day: Int) {
+data class RevDate(val year: Int, val month: RevMonth, val day: Int) {
     companion object {
+        // Convert the despised reactionary date of the Ancien Régime
+        // to our glorious rational format
         fun fromGregorian(date: LocalDate): RevDate {
-            return RevDate(60, 11, 11)
+            val start = LocalDate.of(1792, 9, 22)
+            var days = start.datesUntil(date).count().toInt()
+            var year = 1
+            var yearLength = length(year)
+
+            while (days >= yearLength) {
+                days -= yearLength
+                year += 1
+                yearLength = length(year)
+            }
+            val month = 1 + (days / 30)
+            val day = 1 + (days % 30)
+
+            return RevDate(year, RevMonth.from(month), day)
+        }
+
+        // historical leap years as 3, 7, 11, 15, and 20, and to
+        // treat subsequent years divisible by 4 that aren't divisible by 100
+        // (unless they are also divisible by 400), as per the Gregorian method
+        private fun isRepublicanLeap(year: Int): Boolean {
+            return (year == 3 || year == 7 || year == 11 || year == 15 || year == 20) ||
+                    (year > 20 && (year % 4 == 0 && (year % 100 > 0) || year % 400 == 0))
+        }
+
+        // Days in a given revolutionary year
+        private fun length(year: Int): Int {
+            if (isRepublicanLeap(year)) {
+                return 366
+            } else {
+                return 365
+            }
         }
     }
 }
--- src/test/kotlin/com/jocmp/revcal/RevDateTest.kt
+++ src/test/kotlin/com/jocmp/revcal/RevDateTest.kt
@@ -6,10 +6,26 @@ import kotlin.test.assertEquals
 
 internal class RevDateTest {
     @Test
+    fun `Test Coup of 18 Brumaire`() {
+        val date = RevDate.fromGregorian(LocalDate.of(1799, 11, 9))
+        val expected = RevDate(8, RevMonth.BRUMAIRE, 18)
+
+        assertEquals(expected, date)
+    }
+
+    @Test
+    fun `Test July Revolution`() {
+        val date = RevDate.fromGregorian(LocalDate.of(1830, 7, 26))
+        val expected = RevDate(38, RevMonth.THERMIDOR, 7)
+
+        assertEquals(expected, date)
+    }
+
+    @Test
     fun `Test election of Napolean III`() {
         // https://en.wikipedia.org/wiki/1851_French_coup_d%27%C3%A9tat
         val date = RevDate.fromGregorian(LocalDate.of(1851, 12, 2))
-        val expected = RevDate(60, 11, 11)
+        val expected = RevDate(60, RevMonth.FRIMAIRE, 11)
 
         assertEquals(expected, date)
     }
